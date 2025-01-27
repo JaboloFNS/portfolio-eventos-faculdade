da183ef7dfb67fe44522610c95646bc2158de75a
Fabiano <fabiano.novais.99@gmail.com>
2025-01-27 19:10:14 -0300

    Tela de login de usuários concluída, lista de eventos criada e primeiras visualizações dela disponíveis

--- eventos_unifecaf.py
+++ eventos_unifecaf.py
@@ -12,9 +12,9 @@ def pause():
 aluno = {
     #Dentro desse dicionário o número identificador equivale ao número do RA do aluno, 
     #a partir dele tempos acesso ao restante das informações.
-    "001":{ "Nome": "Ednaldo Pereira", "Nascimento": date(1987, 1, 5), "Curso": "ADS", "Senha": "04123759"},
-    "002":{ "Nome": "Fernanda Rodrigues", "Nascimento": date(1992, 11, 28), "Curso": "GTI", "Senha": "01054764"},
-    "003":{ "Nome": "Rodrigo Amadeu", "Nascimento": date(2001, 4, 25), "Curso": "ADM", "Senha": "47856124"}
+    "001":{ "Nome": "Ednaldo Pereira", "Nascimento": date(1987, 1, 5), "Curso": "ADS", "Senha": "0123"},
+    "002":{ "Nome": "Fernanda Rodrigues", "Nascimento": date(1992, 11, 28), "Curso": "GTI", "Senha": "456"},
+    "003":{ "Nome": "Rodrigo Amadeu", "Nascimento": date(2001, 4, 25), "Curso": "ADM", "Senha": "789"}
 }
 
 def acesso_aluno():
@@ -48,15 +48,48 @@ def acesso_aluno():
     pause()
 
 
+def inscricao_aluno_evento():
+ print()
+        
+
 def menu_aluno():
     print(f"{Fore.BLUE}------------------------------------------------------")
     print(f"{Fore.BLUE}--------- PORTAL DO ALUNO - GRANDES EVENTOS ----------")
-    print(f"{Fore.BLUE}------------------------------------------------------","\n")
+    print(f"{Fore.BLUE}------------------------------------------------------","\n \n \n")
+    lista_eventos()
+    resposta = input("Deseja se cadastrar em um evento? [s/n]")
+    resposta.lower()
+    if resposta == "s" and len(resposta) == 1:
+        inscricao_aluno_evento()
     pause()
 
+#--------------------------------- REFERENTE AO ARMAZENAMENTO E VISUALIZAÇÃO DE EVENTOS ------------------------------
+
+eventos = { "1":{"Descrição": "Workshop - Lógica Computacional com Python", "Data-inicio":(2025,2,25,8,00),
+    "Data-fim":(2025,2,25,12,00), "Professor":"Irineu da Silva Sauro","Local":"Campus Centro - Lab 1", "Vagas Disponíveis": 18,
+    "Alunos Cadastrados": ["003","002"]},
+    "2":{"Descrição": "Workshop - Banco de Dados Relacionais com MySQL Server", "Data-inicio":(2025,3,24,8,00),
+    "Data-fim":(2025,3,24,12,00), "Professor":"Joel Santana","Local":"Campus Centro - Lab 2", "Vagas Disponíveis": 49,
+    "Alunos Cadastrados": ["003"]},
+    "3":{"Descrição": "Palestra - Desenvolvimento Pessoal e Empregabilidade", "Data-inicio":(2025,3,24,8,00),
+    "Data-fim":(2025,3,24,12,00), "Professor":"Maria Gadu","Local":"Campus Taboão Centro - Lab 11", "Vagas Disponíveis": 50,
+    "Alunos Cadastrados": []},
+}
+
+
+def lista_eventos ():
+    print(f"{Fore.GREEN}--------- EVENTOS DISPONÍVEIS ----------")
+    for id_evento, dados_evento  in eventos.items():
+        print(f"{Fore.YELLOW}---------------------------------------------")
+        print(f"Código do Evento: {id_evento}")
+        for chave, valor in dados_evento.items():
+            if chave != "Alunos Cadastrados":
+                print (f"{chave}: {valor}")
 
 
 
+
+#------------------------------ REFERENTE AO MENU INICIAL ------------------------------------
 
 def menu_inicial():
     #O Menu inicial é a primeira tela a ser mostrada quando o usuário abrir o sistema,
